618119f11eb6a6c2635e240dd805210cc387a9b4
tupac <tupac@DESKTOP-M8MUF63>
2022-03-23 20:02:57 +0300

    Добавьте файлы проекта.

---
+++ train/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("123");
+//string? name = Console.ReadLine();
+//Console.WriteLine(name);
+//Console.WriteLine($"Hello, { name }")
+Console.WriteLine(1.2E10);
+
+
+Console.WriteLine("name: ");
+var name = Console.ReadLine();
+Console.WriteLine("heigt: ");
+var height = Convert.ToInt16(Console.ReadLine());
+int wingspan = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Name:{0}, height:{1}, wingspan:{2}", name, height, wingspan);
+
